112b30f4aadf4a71f273e2ad1dc4b5f927858c9a
bourbon <usr.bin.bourbon@gmail.com>
2019-04-22 22:43:33 -0500

    version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Delirium',
-    version='0.0rc1',
+    version='0.1',
     description='Delirium fake DNS server',
     license='LICENSE',
     packages=find_packages(),
