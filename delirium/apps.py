416b2b3b5870c35c878ff0e73f0c3180f8677109
Jason Lewis <jason.lewis@route6security.com>
2019-12-17 06:58:14 -0600

    Replaced argparse' with Click
    Redesigned command line interface with Click instead of argparse.

--- delirium/apps.py
+++ delirium/apps.py
@@ -1,32 +1,53 @@
+import click
+
 from .const import *
 from .dns import FakeDNSServer
 
+@click.group()
+def delirium():
+    pass
 
-def cli_app():
-    import argparse
-    p = argparse.ArgumentParser()
-    p.add_argument('-l', '--listen', default=DEFAULT_LISTEN_ADDR, help='Address to listen on')
-    p.add_argument('-p', '--port', default=DEFAULT_LISTEN_PORT, type=int, help='Port to listen on (UDP)')
-    p.add_argument('-t', '--time', default=DEFAULT_CACHE_DURATION, type=int,
-                   help='Seconds for which cache entries should exist')
-    p.add_argument('-r', '--range', default=DEFAULT_ADDR_RANGE, help='Range of IP addresses to randomly generate')
-    p.add_argument('--db-path', help='Path to sqlite3 database')
-
-    args = p.parse_args()
-
-    if args.db_path:  # there's probably a better way to do this
-        cache_type = CACHE_TYPE.DATABASE
-    else:
-        cache_type = CACHE_TYPE.DICTIONARY
-
+@delirium.command()
+@click.option('--listen', default=DEFAULT_LISTEN_ADDR, help='Address to listen on')
+@click.option('--port', default=DEFAULT_LISTEN_PORT, type=int, help='Port to listen on (UDP)')
+@click.option('--time', default=DEFAULT_CACHE_DURATION, type=int, help='Seconds for which cache entries should exist')
+@click.option('--range', default=DEFAULT_ADDR_RANGE, help='Range of IP addresses to randomly generate')
+@click.option('--db-path', deafult=CACHE_TYPE.DICTIONARY, help='Path to sqlite3 database')
+def dns():
+    click.echo('Running Delirium DNS Server')
     s = FakeDNSServer(args.listen, args.port, args.time, args.range, cache_type, args.db_path)
-
     try:
         s.start()
     except KeyboardInterrupt:
         pass
     finally:
         s.stop()
+
+#def cli_app():
+#    import argparse
+#    p = argparse.ArgumentParser()
+#    p.add_argument('-l', '--listen', default=DEFAULT_LISTEN_ADDR, help='Address to listen on')
+#    p.add_argument('-p', '--port', default=DEFAULT_LISTEN_PORT, type=int, help='Port to listen on (UDP)')
+#    p.add_argument('-t', '--time', default=DEFAULT_CACHE_DURATION, type=int,
+#                   help='Seconds for which cache entries should exist')
+#    p.add_argument('-r', '--range', default=DEFAULT_ADDR_RANGE, help='Range of IP addresses to randomly generate')
+#    p.add_argument('--db-path', help='Path to sqlite3 database')
+#
+#    args = p.parse_args()
+#
+#    if args.db_path:  # there's probably a better way to do this
+#        cache_type = CACHE_TYPE.DATABASE
+#    else:
+#        cache_type = CACHE_TYPE.DICTIONARY
+#
+#    s = FakeDNSServer(args.listen, args.port, args.time, args.range, cache_type, args.db_path)
+#
+#    try:
+#        s.start()
+#    except KeyboardInterrupt:
+#        pass
+#    finally:
+#        s.stop()
 
 if __name__ == "__main__":
     cli_app()
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         'dnslib',
         'ipaddress',
+        'click',
     ],
     entry_points={
         'console_scripts': [
