0ff8bae02c1f57c92db23776fb22a0add6cd2f00
Jason Lewis <jason.lewis@route6security.com>
2019-12-17 14:12:52 -0600

    Update apps.py

--- delirium/apps.py
+++ delirium/apps.py
@@ -30,31 +30,5 @@ def dns(listen, port, time, range, cache_type, db_path):
 
 delirium.add_command(dns)
 
-#def cli_app():
-#    import argparse
-#    p = argparse.ArgumentParser()
-#    p.add_argument('-l', '--listen', default=DEFAULT_LISTEN_ADDR, help='Address to listen on')
-#    p.add_argument('-p', '--port', default=DEFAULT_LISTEN_PORT, type=int, help='Port to listen on (UDP)')
-#    p.add_argument('-t', '--time', default=DEFAULT_CACHE_DURATION, type=int,
-#                   help='Seconds for which cache entries should exist')
-#    p.add_argument('-r', '--range', default=DEFAULT_ADDR_RANGE, help='Range of IP addresses to randomly generate')
-#    p.add_argument('--db-path', help='Path to sqlite3 database')
-#
-#    args = p.parse_args()
-#
-#    if args.db_path:  # there's probably a better way to do this
-#        cache_type = CACHE_TYPE.DATABASE
-#    else:
-#        cache_type = CACHE_TYPE.DICTIONARY
-#
-#    s = FakeDNSServer(args.listen, args.port, args.time, args.range, cache_type, args.db_path)
-#
-#    try:
-#        s.start()
-#    except KeyboardInterrupt:
-#        pass
-#    finally:
-#        s.stop()
-
 if __name__ == "__main__":
     delirium()
