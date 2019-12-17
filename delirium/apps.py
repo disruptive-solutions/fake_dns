aed272e2efc826358b496ce9ea60a6d701509cff
Jason Lewis <jason.lewis@route6security.com>
2019-12-17 14:08:27 -0600

    Fixed Backend
    Click was implemented in the setup.py file and the fakednsserver call was corrected.

--- delirium/apps.py
+++ delirium/apps.py
@@ -7,21 +7,28 @@ from .dns import FakeDNSServer
 def delirium():
     pass
 
-@delirium.command()
+@click.command()
 @click.option('--listen', default=DEFAULT_LISTEN_ADDR, help='Address to listen on')
 @click.option('--port', default=DEFAULT_LISTEN_PORT, type=int, help='Port to listen on (UDP)')
 @click.option('--time', default=DEFAULT_CACHE_DURATION, type=int, help='Seconds for which cache entries should exist')
 @click.option('--range', default=DEFAULT_ADDR_RANGE, help='Range of IP addresses to randomly generate')
-@click.option('--db-path', deafult=CACHE_TYPE.DICTIONARY, help='Path to sqlite3 database')
-def dns():
+@click.option('--cache_type', default=CACHE_TYPE.DICTIONARY, help='Cache type.')
+@click.option('--db-path', help='Path to sqlite3 database')
+def dns(listen, port, time, range, cache_type, db_path):
     click.echo('Running Delirium DNS Server')
-    s = FakeDNSServer(args.listen, args.port, args.time, args.range, cache_type, args.db_path)
+    s = FakeDNSServer(listen, port, time, range, cache_type, db_path)
+    if db_path:  # there's probably a better way to do this
+        cache_type = CACHE_TYPE.DATABASE
+    else:
+        cache_type = CACHE_TYPE.DICTIONARY
     try:
         s.start()
     except KeyboardInterrupt:
         pass
     finally:
         s.stop()
+
+delirium.add_command(dns)
 
 #def cli_app():
 #    import argparse
@@ -50,4 +57,4 @@ def dns():
 #        s.stop()
 
 if __name__ == "__main__":
-    cli_app()
+    delirium()
