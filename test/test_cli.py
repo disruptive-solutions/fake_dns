36d0f4af26512305b8219ee562188db9d780b947
Jason Lewis <jason.lewis@route6security.com>
2020-01-09 11:23:25 -0600

    Created CLI tests
    Tests created for:
    
    --help
    No args
    random string

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,12 +1,32 @@
-from delirium.apps import dns
-from click.testing import CliRunner
 import pytest
-import mock
+from os import getpid
+from os import kill
+from time import sleep
+import threading
 
-"""
-# gets hung up
-def test_apps_loaded():
-    testrun = CliRunner()
-    result = testrun.invoke(dns)
-    assert result.exit_code == 0
-"""
+from click.testing import CliRunner
+
+from delirium.apps import dns
+
+runner = CliRunner()
+
+def test_cli_help():
+    response = runner.invoke(dns, ['--help'])
+    assert 'Show this message and exit.' in response.output
+
+def test_cli_bad_arg():
+    response = runner.invoke(dns, ['oiuytrewqasdfghjkmnbvc'])
+    assert 'Got unexpected extra argument' in response.output
+
+def test_cli_no_args():
+    pid = getpid()
+
+    def trigger_signal():
+        while len(mock_print.mock_calls) < 1:
+            time.sleep(0.2)
+        os.kill(pid, signal.SIGINT)
+
+    thread = threading.Thread(target=trigger_signal)
+    #thread.daemon = True
+    thread.start()
+    assert thread.is_alive() == True
