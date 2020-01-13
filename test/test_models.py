7abbca6b3fec58aee8aa166b19f79228580d6836
Jason Lewis <jason.lewis@route6security.com>
2020-01-13 13:59:37 -0600

    Implement Fixtures
    Implemented fixtures in test_models and test_server to streamline updates.

--- test/test_models.py
+++ test/test_models.py
@@ -6,44 +6,43 @@ from delirium.const import *
 from delirium.dns.cache import get_addr_range
 from delirium.dns.cache import CacheDatabase
 
+@pytest.fixture()
+def test_db():
+    yield CacheDatabase(DEFAULT_ADDR_RANGE, DEFAULT_CACHE_DURATION, DEFAULT_DB_PATH)
 
+@pytest.mark.usefixtures('test_db')
 class TestCacheDatabase():
     TEST_HOST = 'www.somedomain.tld'
 
-    def test_cache_init(self):
-        c = CacheDatabase(DEFAULT_ADDR_RANGE, DEFAULT_CACHE_DURATION, DEFAULT_DB_PATH)
+    def test_cache_init(self, test_db):
+        assert test_db.addr_range == get_addr_range(DEFAULT_ADDR_RANGE)
+        assert test_db.duration == DEFAULT_CACHE_DURATION
 
-        assert c.addr_range == get_addr_range(DEFAULT_ADDR_RANGE)
-        assert c.duration == DEFAULT_CACHE_DURATION
-
-    def test_cache_update(self):
-        c = CacheDatabase(DEFAULT_ADDR_RANGE, DEFAULT_CACHE_DURATION, DEFAULT_DB_PATH)
-
+    def test_cache_update(self, test_db):
         new_dur = 500
-        c.duration = new_dur
-        assert c.duration == new_dur
+        test_db.duration = new_dur
+        assert test_db.duration == new_dur
 
         new_addr_range = '192.168.0.0-192.168.0.255'
-        c.addr_range = new_addr_range
-        assert c.addr_range == get_addr_range(new_addr_range)
+        test_db.addr_range = new_addr_range
+        assert test_db.addr_range == get_addr_range(new_addr_range)
 
-    def test_cache_duration(self):
-        c = CacheDatabase(DEFAULT_ADDR_RANGE, 1, DEFAULT_DB_PATH)
-        c.add_record(self.TEST_HOST)
+    def test_cache_duration(self, test_db):
+        test_db.duration = 1
+        test_db.add_record(self.TEST_HOST)
 
-        assert c.get_addr_by_name(self.TEST_HOST)[0] > 0  
+        assert test_db.get_addr_by_name(self.TEST_HOST)[0] > 0
 
         time.sleep(1)
-        c.prune_stale()
+        test_db.prune_stale()
 
-        assert len(c.get_addr_by_name(self.TEST_HOST)) == 0
+        assert len(test_db.get_addr_by_name(self.TEST_HOST)) == 0
 
-    def test_cache_range(self):
-        c = CacheDatabase(DEFAULT_ADDR_RANGE, DEFAULT_CACHE_DURATION, DEFAULT_DB_PATH)
-        c.add_record(self.TEST_HOST)
-        addr = c.get_addr_by_name(self.TEST_HOST)[0]
+    def test_cache_range(self, test_db):
+        test_db.add_record(self.TEST_HOST)
+        addr = test_db.get_addr_by_name(self.TEST_HOST)[0]
 
-        assert (c.addr_range[0] <= addr <= c.addr_range[1]) == True
+        assert (test_db.addr_range[0] <= addr <= test_db.addr_range[1]) == True
 
 
 if __name__ == '__main__':
--- test/test_server.py
+++ test/test_server.py
@@ -16,6 +16,9 @@ def _get_unused_udp_port():
     s.close()
     return port
 
+@pytest.fixture()
+def test_server():
+    yield FakeDNSServer(port=7357)
 
 def _is_valid_ip_addr(addr):
     try:
@@ -29,42 +32,38 @@ def _is_valid_ip_addr(addr):
 class TestFakeDNSServer():
     PORT = _get_unused_udp_port()  # can't bind to anything south of 1024 without root (default 53)
 
-    def test_server_init(self):
-        s = FakeDNSServer(port=self.PORT)
-
-        assert s.port == self.PORT
-        assert s.addr == DEFAULT_LISTEN_ADDR
-        assert s.addr_range == get_addr_range(DEFAULT_ADDR_RANGE)
-        assert s.duration == DEFAULT_CACHE_DURATION
+    def test_server_init(self, test_server):
+        assert test_server.port == 7357
+        assert test_server.addr == DEFAULT_LISTEN_ADDR
+        assert test_server.addr_range == get_addr_range(DEFAULT_ADDR_RANGE)
+        assert test_server.duration == DEFAULT_CACHE_DURATION
 
     # noinspection PyPropertyAccess
-    def test_server_update(self):
+    def test_server_update(self, test_server):
         s = FakeDNSServer(port=self.PORT)
 
         # reallu updating the FakeCache object but the server will proxy these
         new_dur = 500
-        s.duration = new_dur
-        assert s.duration == new_dur
+        test_server.duration = new_dur
+        assert test_server.duration == new_dur
 
         new_addr_range = '192.168.0.0-192.168.0.255'
-        s.addr_range = new_addr_range
-        assert s.addr_range == get_addr_range(new_addr_range)
+        test_server.addr_range = new_addr_range
+        assert test_server.addr_range == get_addr_range(new_addr_range)
 
         with pytest.raises(AttributeError):
-            s.cache = {}
+            test_server.cache = {}
 
         with pytest.raises(AttributeError):
-            s.port = _get_unused_udp_port()
+            test_server.port = _get_unused_udp_port()
 
         with pytest.raises(AttributeError):
-            s.addr = '192.168.0.100'
+            test_server.addr = '192.168.0.100'
 
-    def test_server_start(self):
-        # TODO: this could probably be improved
-        s = FakeDNSServer(port=self.PORT)
-        s.start_thread()
-        assert s.is_alive() == True
-        s.stop()
+    def test_server_start(self, test_server):
+        test_server.start_thread()
+        assert test_server.is_alive() == True
+        test_server.stop()
 
 
 if __name__ == '__main__':
